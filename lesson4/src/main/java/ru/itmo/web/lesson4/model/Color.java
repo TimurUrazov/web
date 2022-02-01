8165ad2e052a3ff324257d4da846fff019f74303
TimurUrazov <tu177086@gmail.com>
2022-02-01 08:39:03 +0300

    previous naming was removed

--- lesson4/src/main/java/ru/itmo/web/lesson4/model/Color.java
+++
@@ -1,5 +0,0 @@
-package ru.itmo.web.lesson4.model;
-
-public enum Color {
-    RED, GREEN, BLUE
-}
--- lesson5/src/main/java/ru/itmo/wp/web/page/NotFoundPage.java
+++
@@ -1,8 +0,0 @@
-package ru.itmo.wp.web.page;
-
-@SuppressWarnings({"unused", "RedundantSuppression"})
-public class NotFoundPage {
-    private void action() {
-        // No operations.
-    }
-}
